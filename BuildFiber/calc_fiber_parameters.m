eecbd9f13f340f6e2133fdd8e0fe95f26020d022
AaHaHaa <46036069+AaHaHaa@users.noreply.github.com>
2023-07-05 01:52:13 -0400

    Add files via upload

---
+++ BuildFiber/calc_fiber_parameters.m
@@ -0,0 +1,23 @@
+clearvars; close all;
+
+addpath('helpers/');
+
+%% Set parameters (users modify only this block)
+modes_used = 1; % In general the modes do not have to be consecutive
+Nx = 400; % number of spatial grid points for each mode
+gpu_yes = true; % true = run on GPU, false = run on CPU
+folder_name = '1060XP_wavelength1030nm'; % folder containing the calculated modes
+
+% File name parameters:
+Nf = 10;
+lambda0 = 1030e-9; % center wavelength in nm; an "integer"
+freq_range = 100; % THz; frequency range. Should be the same as the range used to calculate the modes. Usually 100 THz.
+bandwidth = 'narrowband'; % 'narrowband' or 'broadband'
+
+polynomial_fit_order = 7;
+num_disp_orders = 5; % i.e. if this is 3, four coefficients will be calculated, including the 0th order
+
+%%
+calc_dispersion(modes_used,folder_name,Nf,lambda0,freq_range,bandwidth,polynomial_fit_order,num_disp_orders);
+calc_SRSK_tensors(modes_used,Nx,gpu_yes,folder_name,lambda0);
+delete_modeprofiles(modes_used,folder_name,Nf,lambda0,freq_range)
