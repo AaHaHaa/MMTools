ad20971d92ccade634227d0c57356c3f334cd4df
AaHaHaa <46036069+AaHaHaa@users.noreply.github.com>
2024-01-17 11:20:44 -0500

    Add files via upload

---
+++ GMMNLSE/Examples/Some tutorials/7. Broadband SPM-based supercontinuum/supercontinuum_narrowband_betas.m
@@ -0,0 +1,101 @@
+% This code demonstrates the self-phase modulation of a pulse.
+%
+% This code uses adaptive-step RK4IP for the passive-fiber propagation.
+
+close all; clearvars;
+
+addpath('../../../GMMNLSE algorithm/','../../../user_helpers/');
+
+%% Setup fiber parameters
+sim.lambda0 = 1030e-9; % the center wavelength
+sim.gpu_yes = false; % don't use GPU
+
+% Load default parameters like 
+%
+% load fiber.betas and fiber.SR based on your multimode folder above
+% sim.f0 = 3e5/sim.lambda0; THz
+% sim.save_period = 0; Save only the fields at the beginning and the end fiber
+% sim.ellipticity = 0; Linear polarization
+% sim.scalar = true; Use scalar propagation
+% sim.adaptive_deltaZ.threshold = 1e-5; the threshold of the adaptive-step method
+% sim.gpu_yes = true; Use GPU
+% sim.Raman_model = 1; Use the isotropic Raman model
+% sim.gain_model = 0; Don't use gain model = passive propagation
+% sim.pulse_centering = true; Always shift the pulse to the center of the time window
+% sim.num_photon_noise_per_bin = 0; Don't include photon shot noise
+% sim.gpuDevice.Index = 1; Use the GPU device 1
+% sim.progress_bar = true; Show the progress bar
+% sim.progress_bar_name = ''; Empty name for the progress bar
+% sim.cuda_dir_path = 'GMMNLSE/cuda'; Where the cuda files are
+% ......
+%
+% Please check this function for details.
+[fiber,sim] = load_default_GMMNLSE_propagate([],sim); % load default parameters
+
+num_save = 100;
+fiber.L0 = 1;
+fiber.betas = zeros(size(fiber.betas)); % no dispersion
+sim.save_period = fiber.L0/num_save;
+
+%% Setup general parameters
+Nt = 2^14; % the number of time points
+time_window = 30; % ps
+dt = time_window/Nt;
+f = sim.f0+(-Nt/2:Nt/2-1)'/(Nt*dt); % THz
+t = (-Nt/2:Nt/2-1)'*dt; % ps
+c = 299792458; % m/s
+lambda = c./(f*1e12)*1e9; % nm
+
+%% Initial condition
+tfwhm = 2; % ps
+total_energy = 100; % nJ
+initial_pulse = build_MMgaussian(tfwhm, time_window, total_energy, 1, Nt);
+
+%% Propagate
+prop_output = GMMNLSE_propagate( fiber,initial_pulse,sim);
+
+%% Plot
+% Time
+figure;
+h = plot(t,abs(prop_output.fields(:,:,end)).^2);
+xlim([-15,15]);
+xlabel('t');
+ylabel('Power');
+title('Field');
+set(h,'linewidth',2);
+set(gca,'fontsize',14);
+
+% Spectrum
+figure;
+h = plot(f-sim.f0,abs(fftshift(ifft(prop_output.fields(:,:,end)),1)).^2);
+xlim([-80,80]);
+xlabel('\nu-\nu_0');
+ylabel('PSD');
+title('Spectrum');
+set(h,'linewidth',2);
+set(gca,'fontsize',14);
+
+% Comparison of time
+figure;
+[x,y] = meshgrid(t,prop_output.z);
+pcolor(x,y,permute(abs(prop_output.fields(:,1,:)).^2,[3 1 2]));
+shading interp; colormap(jet);
+xlim([-15,15]);
+xlabel('t');
+ylabel('z');
+title('Field during propagation');
+set(gca,'fontsize',14);
+
+% Comparison of spectra
+figure;
+[x,y] = meshgrid(f-sim.f0,prop_output.z(2:end));
+pcolor(x,y,permute(abs(fftshift(ifft(prop_output.fields(:,1,2:end)),1)).^2,[3 1 2]));
+xlim([-80,80]);
+shading interp; colormap(jet);
+xlabel('\nu-\nu_0');
+ylabel('z/LD');
+title('Spectrum during propagation');
+set(gca,'fontsize',14);
+
+%% Save the data
+%save('supercontinuum.mat');
