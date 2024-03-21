aa8c3e1b9c852c6b1f6f414131d72c564deabf4f
AaHaHaa <46036069+AaHaHaa@users.noreply.github.com>
2024-03-21 00:04:27 -0400

    Add files via upload

--- GMMNLSE/user_helpers/saturable_absorber_action_NPE.m
+++ GMMNLSE/user_helpers/saturable_absorber_action_NPE.m
@@ -5,6 +5,12 @@ function output = saturable_absorber_action_NPE(input, M1, M2, P1, P2)
 %  OR
 %   input - a (N, num_mdoes, m) matrix with the fields for each mode, in the time domain
 %
+% References:
+% 1. Li et al., "Geometrical description of the onset of multi-pulsing in 
+%    mode-locked laser cavities," J. Opt. Soc. Am. B, 27(10), 2068-2077 
+%    (2010)
+% 2. Wei et al., "General description and understanding of the nonlinear
+%    dynamics of mode-locked fiber lasers," Sci. Rep. 7(1), 1292 (2017)
 
 if isstruct(input)
     input_field = input.fields(:, :, end);
@@ -13,7 +19,7 @@ else
 end
 
 powers = abs(input_field).^2;
-output_field = input_field.*sqrt(M1+M2*(1+cos(2*pi*(powers-P1)/P2)));
+output_field = input_field.*sqrt(M1+M2*(1-cos(2*pi*(powers-P1)/P2)));
 
 if isstruct(input)
     output = input;
