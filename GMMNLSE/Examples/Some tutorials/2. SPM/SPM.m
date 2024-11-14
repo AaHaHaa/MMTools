5ae13a496c440b032722e760a96109352c4e502d
AaHaHaa <46036069+AaHaHaa@users.noreply.github.com>
2024-11-14 12:50:45 -0500

    Update readme

--- GMMNLSE/Examples/Some tutorials/2. SPM/SPM.m
+++ GMMNLSE/Examples/Some tutorials/2. SPM/SPM.m
@@ -97,21 +97,5 @@ ylabel('z/LD');
 title('Spectrum during propagation');
 set(gca,'fontsize',14);
 
-pause(1);
-
-% High-resolution spectrogram
-log_yes = false; % Use "log_yes = true" to see, under log scale, how spectral interference generates temporal fringes.
-save_point = size(prop_output.fields,3);
-Frame(save_point) = struct('cdata',[],'colormap',[]);
-for i = 1:save_point
-    [~,~,~,figs,ax] = calc_spectrogram(t,f,prop_output.fields(:,1,i),[-3,3],[940,1120],400,400,true,true,log_yes);
-    set(figs,'Color',[1,1,1]);
-
-    Frame(i) = getframe(figs);
-    close(figs);
-end
-% Movie
-implay(Frame,20);
-
 %% Save the data
 save('SPM.mat');
--- GMMNLSE/Examples/Some tutorials/2. SPM/SPM_plotter.m
+++ GMMNLSE/Examples/Some tutorials/2. SPM/SPM_plotter.m
@@ -45,11 +45,54 @@ ylabel('z/LD');
 title('Spectrum during propagation');
 set(gca,'fontsize',14);
 
-% Movie
-implay(Frame,20);
+% Movie (spectrogram)
+pause(1);
 
-exportVideo = VideoWriter('SPM');
+% High-resolution spectrogram
+log_yes = false; % Use "log_yes = true" to see, under log scale, how spectral interference generates temporal fringes.
+save_point = size(prop_output.fields,3);
+Frame1(save_point) = struct('cdata',[],'colormap',[]);
+for i = 1:save_point
+    [~,~,~,figs,ax] = calc_spectrogram(t,f,prop_output.fields(:,1,i),[-3,3],[940,1120],400,400,true,true,log_yes);
+    set(figs,'Color',[1,1,1]);
+
+    Frame1(i) = getframe(figs);
+    close(figs);
+end
+% Movie
+implay(Frame1,20);
+
+exportVideo = VideoWriter('SPM_spectrogram');
 exportVideo.FrameRate = 20;
 open(exportVideo);
-writeVideo(exportVideo, Frame);
+writeVideo(exportVideo, Frame1);
+close(exportVideo);
+
+% Movie (spectrum)
+pause(1);
+
+spectrum = abs(fftshift(ifft(prop_output.fields),1)).^2;
+spectrum = spectrum/max(spectrum(:));
+Frame2(save_point) = struct('cdata',[],'colormap',[]);
+for i = 1:save_point
+    
+    figs = figure;
+    h = plot(f-sim.f0,spectrum(:,:,i),'linewidth',2,'Color','b');
+    xlim([-20,20]);
+    xlabel('\nu-\nu_0');
+    ylabel('PSD');
+    title('Spectrum');
+    set(gca,'fontsize',20);
+    set(figs,'Color',[1,1,1]);
+
+    Frame2(i) = getframe(figs);
+    close(figs);
+end
+% Movie
+implay(Frame2,20);
+
+exportVideo = VideoWriter('SPM_spectrum');
+exportVideo.FrameRate = 20;
+open(exportVideo);
+writeVideo(exportVideo, Frame2);
 close(exportVideo);
